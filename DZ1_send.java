e9cba99367b0c835066334ab90c24b6210aafd85
DinaraS <dinara6@mail.ru>
2024-09-29 13:39:55 +0300

    Added file DZ1_send.java

---
+++ DZ1_send.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+import java.lang.System;
+
+public class All_Tasks {
+
+public static void main(String[] args) {
+    
+    // factorial();
+    // printEvenNums();
+    // sumDigits();
+
+
+}
+
+// //-------------------------------------------------------------------------------------------------------------
+// // Задание1.Нахождение факториала числа. Напишите метод factorial, который принимает число n и возвращает его факториал.
+// // Есличисло n < 0, метод должен вернуть-1.
+
+// // public static void factorial() {
+// //     Scanner sc = new Scanner(System.in);
+// //     System.out.printf("Input tne number N: ");
+// //     int n = sc.nextInt();
+// //     int i = 1;
+// //     int value = 1;
+
+// // if (n >= 0) {
+// //     while (i <= n) {
+// //         value = value * i;
+// //         i = i + 1;
+// //     }
+// //     sc.close();
+// //     System.out.printf("Factorial of the number: %d = %d", n, value);
+// // }
+// // if (n < 0) {
+// //     System.out.printf("Factorial of the number: %d = %d", n, -1);
+// // }
+// // }
+// //     }
+
+// //-------------------------------------------------------------------------------------------------------------
+// // Задача2.Вывод всех четных чисел от 1 до 100. Напишите метод printEvenNums, который выведет на экран все четные 
+// // числа в промежутке от 1 до 100, каждое на новой строке.
+
+// // public static void printEvenNums() {
+// //     int[] arr = new int[100];
+// //     for (int i = 0; i < arr.length-1; i++) {
+// //         if (arr[i] % 2 == 0) {
+// //             arr[i] = i;
+// //             System.out.println(arr[i]);
+// //         }
+// //         i = i + 1;
+// //    }
+// // }
+
+// //-------------------------------------------------------------------------------------------------------------
+// // Задача3.Подсчет суммы цифр числа. Напишите метод sumDigits, который принимает целое число n и возвращает сумму 
+// // его цифр.
+
+// // public static void sumDigits() {
+
+// //     Scanner sc = new Scanner(System.in);
+// //     System.out.printf("Input a triple digits: ");
+// //     String num2 = sc.nextLine();
+// //     sc.close();
+// //     int num = Integer.parseInt(num2);
+
+// //         if (num2.length() == 3) {
+            
+// //                 int first = num % 10;
+// //                 int second = num /10 % 10;
+// //                 int third = num / 100;   
+// //                 int sum = first + second + third;
+
+// //                 System.out.printf("The sum of a triple digits %d = " + sum, num);
+// //             }
+// //                 else {
+// //                 System.out.println("You input not three-digit number");
+// //                 }
+// //         }
+    
+// }
+
+}
